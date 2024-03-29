10f8218dab8c471f83f6fe609488e103dca7865b
bainaifu <65045946+bainaifu@users.noreply.github.com>
2024-03-28 19:45:33 -0500

    Update CartView.js

--- assignment2/src/CartView.js
+++ assignment2/src/CartView.js
@@ -61,7 +61,7 @@ export function Cart() {
                 <div class="row g-3">
                   <div class="col-sm-6">
                     <label for="firstName" class="form-label">First name</label>
-                    <input type="text" class="form-control" id="firstName" placeholder="" value="" required>
+                    <input type="text" class="form-control" id="firstName" placeholder="" value="" required />
                     <div class="invalid-feedback">
                       Valid first name is required.
                     </div>
@@ -69,7 +69,7 @@ export function Cart() {
       
                   <div class="col-sm-6">
                     <label for="lastName" class="form-label">Last name</label>
-                    <input type="text" class="form-control" id="lastName" placeholder="" value="" required>
+                    <input type="text" class="form-control" id="lastName" placeholder="" value="" required />
                     <div class="invalid-feedback">
                       Valid last name is required.
                     </div>
@@ -79,7 +79,7 @@ export function Cart() {
                     <label for="username" class="form-label">Username</label>
                     <div class="input-group has-validation">
                       <span class="input-group-text">@</span>
-                      <input type="text" class="form-control" id="username" placeholder="Username" required>
+                      <input type="text" class="form-control" id="username" placeholder="Username" required />
                     <div class="invalid-feedback">
                         Your username is required.
                       </div>
@@ -88,7 +88,7 @@ export function Cart() {
       
                   <div class="col-12">
                     <label for="email" class="form-label">Email <span class="text-body-secondary">(Optional)</span></label>
-                    <input type="email" class="form-control" id="email" placeholder="you@example.com">
+                    <input type="email" class="form-control" id="email" placeholder="you@example.com" />
                     <div class="invalid-feedback">
                       Please enter a valid email address for shipping updates.
                     </div>
@@ -96,7 +96,7 @@ export function Cart() {
       
                   <div class="col-12">
                     <label for="address" class="form-label">Address</label>
-                    <input type="text" class="form-control" id="address" placeholder="1234 Main St" required>
+                    <input type="text" class="form-control" id="address" placeholder="1234 Main St" required />
                     <div class="invalid-feedback">
                       Please enter your shipping address.
                     </div>
@@ -104,7 +104,7 @@ export function Cart() {
       
                   <div class="col-12">
                     <label for="address2" class="form-label">Address 2 <span class="text-body-secondary">(Optional)</span></label>
-                    <input type="text" class="form-control" id="address2" placeholder="Apartment or suite">
+                    <input type="text" class="form-control" id="address2" placeholder="Apartment or suite" />
                   </div>
       
                   <div class="col-md-5">
@@ -131,7 +131,7 @@ export function Cart() {
       
                   <div class="col-md-3">
                     <label for="zip" class="form-label">Zip</label>
-                    <input type="text" class="form-control" id="zip" placeholder="" required>
+                    <input type="text" class="form-control" id="zip" placeholder="" required />
                     <div class="invalid-feedback">
                       Zip code required.
                     </div>
@@ -141,12 +141,12 @@ export function Cart() {
                 <hr class="my-4"></hr>
       
                 <div class="form-check">
-                  <input type="checkbox" class="form-check-input" id="same-address">
+                  <input type="checkbox" class="form-check-input" id="same-address" />
                   <label class="form-check-label" for="same-address">Shipping address is the same as my billing address</label>
                 </div>
       
                 <div class="form-check">
-                  <input type="checkbox" class="form-check-input" id="save-info">
+                  <input type="checkbox" class="form-check-input" id="save-info"/>
                   <label class="form-check-label" for="save-info">Save this information for next time</label>
                 </div>
       
@@ -156,15 +156,15 @@ export function Cart() {
       
                 <div class="my-3">
                   <div class="form-check">
-                    <input id="credit" name="paymentMethod" type="radio" class="form-check-input" checked required>
+                    <input id="credit" name="paymentMethod" type="radio" class="form-check-input" checked required />
                     <label class="form-check-label" for="credit">Credit card</label>
                   </div>
                   <div class="form-check">
-                    <input id="debit" name="paymentMethod" type="radio" class="form-check-input" required>
+                    <input id="debit" name="paymentMethod" type="radio" class="form-check-input" required />
                     <label class="form-check-label" for="debit">Debit card</label>
                   </div>
                   <div class="form-check">
-                    <input id="paypal" name="paymentMethod" type="radio" class="form-check-input" required>
+                    <input id="paypal" name="paymentMethod" type="radio" class="form-check-input" required />
                     <label class="form-check-label" for="paypal">PayPal</label>
                   </div>
                 </div>
@@ -172,7 +172,7 @@ export function Cart() {
                 <div class="row gy-3">
                   <div class="col-md-6">
                     <label for="cc-name" class="form-label">Name on card</label>
-                    <input type="text" class="form-control" id="cc-name" placeholder="" required>
+                    <input type="text" class="form-control" id="cc-name" placeholder="" required />
                     <small class="text-body-secondary">Full name as displayed on card</small>
                     <div class="invalid-feedback">
                       Name on card is required
@@ -181,7 +181,7 @@ export function Cart() {
       
                   <div class="col-md-6">
                     <label for="cc-number" class="form-label">Credit card number</label>
-                    <input type="text" class="form-control" id="cc-number" placeholder="" required>
+                    <input type="text" class="form-control" id="cc-number" placeholder="" required />
                     <div class="invalid-feedback">
                       Credit card number is required
                     </div>
@@ -189,7 +189,7 @@ export function Cart() {
       
                   <div class="col-md-3">
                     <label for="cc-expiration" class="form-label">Expiration</label>
-                    <input type="text" class="form-control" id="cc-expiration" placeholder="" required>
+                    <input type="text" class="form-control" id="cc-expiration" placeholder="" required />
                     <div class="invalid-feedback">
                       Expiration date required
                     </div>
@@ -197,7 +197,7 @@ export function Cart() {
       
                   <div class="col-md-3">
                     <label for="cc-cvv" class="form-label">CVV</label>
-                    <input type="text" class="form-control" id="cc-cvv" placeholder="" required>
+                    <input type="text" class="form-control" id="cc-cvv" placeholder="" required />
                     <div class="invalid-feedback">
                       Security code required
                     </div>
@@ -212,4 +212,4 @@ export function Cart() {
           </div>
       </div>
     );
-  }
+};
