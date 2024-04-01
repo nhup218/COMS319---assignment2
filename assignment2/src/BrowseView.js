09607101992bfbb3decec92d59b93cbf98113a7c
Nhu Phan <nhup218@gmail.com>
2024-03-31 20:50:55 -0500

    handled cvv and expiration date

--- assignment2/src/BrowseView.js
+++ assignment2/src/BrowseView.js
@@ -70,123 +70,121 @@ export function BrowseView({
         href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css"
       ></link>
       <div className="album py-5 bg-body-tertiary">
-        <div className="col-span-1 p-4">
-          <input
-            type="text"
-            name="price"
-            id="price"
-            className="block rounded-md border-0 py-2 pl-3 pr-10 px-4 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            placeholder="Search"
-            onChange={handleSearchChange}
-          />
-        </div>
-        <div class="album py-5 bg-body-tertiary">
-          <div className="container">
-            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-              {filtered.map((product) => (
-                <div className="col">
-                  <div key={product.id}>
-                    <div className="card shadow-sm">
-                      <img
-                        src={product.image}
-                        alt={product.title}
-                        width="100%"
-                        height="225"
-                      ></img>
-                      <div className="card-body">
-                        <h4 className="card-text">{product.title}</h4>
-                        <p className="card-text">${product.price}</p>
-                        <div className="d-flex justify-content-between align-items-center">
-                          <div className="btn-group">
-                            <button
-                              type="button"
-                              className="btn btn-sm btn-outline-secondary"
-                              onClick={() => addToCart(product.title)}
-                            >
-                              +
-                            </button>
-                            <span style={{ color: "black" }}>
-                              &emsp; {cart[product.title]} &emsp;
-                            </span>
-                            <button
-                              type="button"
-                              className="btn btn-sm btn-outline-secondary"
-                              onClick={() => removeFromCart(product.title)}
-                            >
-                              -
-                            </button>
-                          </div>
+        <div className="container">
+          <div className="col-span-1 p-4">
+            <input
+              type="text"
+              name="price"
+              id="price"
+              className="block rounded-md border-0 py-2 pl-3 pr-10 px-4 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              placeholder="Search"
+              onChange={handleSearchChange}
+            />
+          </div>
+          <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+            {filtered.map((product) => (
+              <div className="col">
+                <div key={product.id}>
+                  <div className="card shadow-sm">
+                    <img
+                      src={product.image}
+                      alt={product.title}
+                      width="100%"
+                      height="225"
+                    ></img>
+                    <div className="card-body">
+                      <h4 className="card-text">{product.title}</h4>
+                      <p className="card-text">${product.price}</p>
+                      <div className="d-flex justify-content-between align-items-center">
+                        <div className="btn-group">
+                          <button
+                            type="button"
+                            className="btn btn-sm btn-outline-secondary"
+                            onClick={() => addToCart(product.title)}
+                          >
+                            +
+                          </button>
+                          <span style={{ color: "black" }}>
+                            &emsp; {cart[product.title]} &emsp;
+                          </span>
+                          <button
+                            type="button"
+                            className="btn btn-sm btn-outline-secondary"
+                            onClick={() => removeFromCart(product.title)}
+                          >
+                            -
+                          </button>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
-              ))}
-            </div>
-            <div className="col-span-2 p-4 ">
-              <div>
-                <div className="bg-white border-white border-2 p-4 rounded">
-                  <h1>Cart</h1>
-                  <div className="text-left">
-                    {Object.keys(cart).map((key) =>
-                      cart[key] > 0 ? (
-                        <div key={key}>
-                          {key}: {cart[key]} x ${prices[key].toFixed(2)}
-                        </div>
-                      ) : null
-                    )}
-                  </div>
-                  <br></br>
-                  <div>
-                    Subtotal: $
-                    {Object.keys(cart)
-                      .map((key) => (cart[key] > 0 ? prices[key] : 0))
-                      .reduce(
-                        (total, price, index) =>
-                          total + price * cart[Object.keys(cart)[index]],
-                        0
-                      )
-                      .toFixed(2)}
-                  </div>
-                  <br></br>
-                  <div>
-                    Tax: $
-                    {Object.keys(cart)
-                      .map((key) => (cart[key] > 0 ? prices[key] : 0))
-                      .reduce(
-                        (total, price, index) =>
-                          total + 0.07 * price * cart[Object.keys(cart)[index]],
-                        0
-                      )
-                      .toFixed(2)}
-                  </div>
-                  <br></br>
-                  <div>
-                    Total: $
-                    {Object.keys(cart)
-                      .map((key) => (cart[key] > 0 ? prices[key] : 0))
-                      .reduce(
-                        (total, price, index) =>
-                          total +
-                          price * cart[Object.keys(cart)[index]] +
-                          0.06 * price * cart[Object.keys(cart)[index]],
-                        0
-                      )
-                      .toFixed(2)}
-                  </div>
+              </div>
+            ))}
+          </div>
+          <div className="col-span-2 p-4 ">
+            <div>
+              <div className="bg-white border-white border-2 p-4 rounded">
+                <h1>Cart</h1>
+                <div className="text-left">
+                  {Object.keys(cart).map((key) =>
+                    cart[key] > 0 ? (
+                      <div key={key}>
+                        {key}: {cart[key]} x ${prices[key].toFixed(2)}
+                      </div>
+                    ) : null
+                  )}
+                </div>
+                <br></br>
+                <div>
+                  Subtotal: $
+                  {Object.keys(cart)
+                    .map((key) => (cart[key] > 0 ? prices[key] : 0))
+                    .reduce(
+                      (total, price, index) =>
+                        total + price * cart[Object.keys(cart)[index]],
+                      0
+                    )
+                    .toFixed(2)}
+                </div>
+                <br></br>
+                <div>
+                  Tax: $
+                  {Object.keys(cart)
+                    .map((key) => (cart[key] > 0 ? prices[key] : 0))
+                    .reduce(
+                      (total, price, index) =>
+                        total + 0.07 * price * cart[Object.keys(cart)[index]],
+                      0
+                    )
+                    .toFixed(2)}
+                </div>
+                <br></br>
+                <div>
+                  Total: $
+                  {Object.keys(cart)
+                    .map((key) => (cart[key] > 0 ? prices[key] : 0))
+                    .reduce(
+                      (total, price, index) =>
+                        total +
+                        price * cart[Object.keys(cart)[index]] +
+                        0.06 * price * cart[Object.keys(cart)[index]],
+                      0
+                    )
+                    .toFixed(2)}
                 </div>
               </div>
-
-              <button
-                id="checkout-button"
-                onClick={() => {
-                  doneShopping();
-                }}
-                className="bg-green-500 hover:bg-green-700 py-4 px-4 border-green-700 rounded"
-              >
-                🛒
-              </button>
             </div>
+
+            <button
+              id="checkout-button"
+              onClick={() => {
+                doneShopping();
+              }}
+              className="bg-green-500 hover:bg-green-700 py-4 px-4 border-green-700 rounded"
+            >
+              🛒
+            </button>
           </div>
         </div>
       </div>
--- assignment2/src/CartView.js
+++ assignment2/src/CartView.js
@@ -91,6 +91,20 @@ function formatCreditCardNumber(input) {
   input.value = formattedNumber.replace(/-$/, "");
 }
 
+function formatExpirationDate(input) {
+  const cardExpiration = input.value.replace(/\D/g, "");
+  const formattedExpiration = cardExpiration.replace(/(\d{2})/g, "$1/");
+  
+  input.value = formattedExpiration.replace(/\/$/, "");
+}
+
+function formatCVV(input) {
+  const cardCVV = input.value.replace(/\D/g, "");
+  
+  input.value = cardCVV;
+}
+
+
 export function CartView({
   isActive,
   changePage,
@@ -112,6 +126,16 @@ export function CartView({
   const handleFormatCardNumber = (event) => {
     const creditCard = document.getElementById("cc-number");
     formatCreditCardNumber(creditCard);
+  }
+
+  const handleFormatCardExpiration = (event) => {
+    const cardExpiration = document.getElementById("cc-expiration");
+    formatExpirationDate(cardExpiration);
+  }
+
+  const handleFormatCVV = (event) => {
+    const cardCVV = document.getElementById("cc-cvv");
+    formatCVV(cardCVV);
   }
 
   const handleSubmit = (event) => {
@@ -266,164 +290,6 @@ export function CartView({
                     Valid first name is required.
                   </div>
                 </div>
-<<<<<<< Updated upstream
-              </form>
-            </div>
-            <div class="col-md-7 col-lg-8">
-              <h4 class="mb-3">Billing address</h4>
-              <form class="needs-validation" novalidate>
-                <div class="row g-3">
-                  <div class="col-sm-6">
-                    <label for="firstName" class="form-label">First name</label>
-                    <input type="text" class="form-control" id="firstName" placeholder="" value="" required />
-                    <div class="invalid-feedback">
-                      Valid first name is required.
-                    </div>
-                  </div>
-      
-                  <div class="col-sm-6">
-                    <label for="lastName" class="form-label">Last name</label>
-                    <input type="text" class="form-control" id="lastName" placeholder="" value="" required />
-                    <div class="invalid-feedback">
-                      Valid last name is required.
-                    </div>
-                  </div>
-      
-                  <div class="col-12">
-                    <label for="username" class="form-label">Username</label>
-                    <div class="input-group has-validation">
-                      <span class="input-group-text">@</span>
-                      <input type="text" class="form-control" id="username" placeholder="Username" required />
-                    <div class="invalid-feedback">
-                        Your username is required.
-                      </div>
-                    </div>
-                  </div>
-      
-                  <div class="col-12">
-                    <label for="email" class="form-label">Email <span class="text-body-secondary">(Optional)</span></label>
-                    <input type="email" class="form-control" id="email" placeholder="you@example.com" />
-                    <div class="invalid-feedback">
-                      Please enter a valid email address for shipping updates.
-                    </div>
-                  </div>
-      
-                  <div class="col-12">
-                    <label for="address" class="form-label">Address</label>
-                    <input type="text" class="form-control" id="address" placeholder="1234 Main St" required />
-                    <div class="invalid-feedback">
-                      Please enter your shipping address.
-                    </div>
-                  </div>
-      
-                  <div class="col-12">
-                    <label for="address2" class="form-label">Address 2 <span class="text-body-secondary">(Optional)</span></label>
-                    <input type="text" class="form-control" id="address2" placeholder="Apartment or suite" />
-                  </div>
-      
-                  <div class="col-md-5">
-                    <label for="country" class="form-label">Country</label>
-                    <select class="form-select" id="country" required>
-                      <option value="">Choose...</option>
-                      <option>United States</option>
-                    </select>
-                    <div class="invalid-feedback">
-                      Please select a valid country.
-                    </div>
-                  </div>
-      
-                  <div class="col-md-4">
-                    <label for="state" class="form-label">State</label>
-                    <select class="form-select" id="state" required>
-                      <option value="">Choose...</option>
-                      <option>California</option>
-                    </select>
-                    <div class="invalid-feedback">
-                      Please provide a valid state.
-                    </div>
-                  </div>
-      
-                  <div class="col-md-3">
-                    <label for="zip" class="form-label">Zip</label>
-                    <input type="text" class="form-control" id="zip" placeholder="" required />
-                    <div class="invalid-feedback">
-                      Zip code required.
-                    </div>
-                  </div>
-                </div>
-      
-                <hr class="my-4"></hr>
-      
-                <div class="form-check">
-                  <input type="checkbox" class="form-check-input" id="same-address" />
-                  <label class="form-check-label" for="same-address">Shipping address is the same as my billing address</label>
-                </div>
-      
-                <div class="form-check">
-                  <input type="checkbox" class="form-check-input" id="save-info"/>
-                  <label class="form-check-label" for="save-info">Save this information for next time</label>
-                </div>
-      
-                <hr class="my-4"></hr>
-      
-                <h4 class="mb-3">Payment</h4>
-      
-                <div class="my-3">
-                  <div class="form-check">
-                    <input id="credit" name="paymentMethod" type="radio" class="form-check-input" checked required />
-                    <label class="form-check-label" for="credit">Credit card</label>
-                  </div>
-                  <div class="form-check">
-                    <input id="debit" name="paymentMethod" type="radio" class="form-check-input" required />
-                    <label class="form-check-label" for="debit">Debit card</label>
-                  </div>
-                  <div class="form-check">
-                    <input id="paypal" name="paymentMethod" type="radio" class="form-check-input" required />
-                    <label class="form-check-label" for="paypal">PayPal</label>
-                  </div>
-                </div>
-      
-                <div class="row gy-3">
-                  <div class="col-md-6">
-                    <label for="cc-name" class="form-label">Name on card</label>
-                    <input type="text" class="form-control" id="cc-name" placeholder="" required />
-                    <small class="text-body-secondary">Full name as displayed on card</small>
-                    <div class="invalid-feedback">
-                      Name on card is required
-                    </div>
-                  </div>
-      
-                  <div class="col-md-6">
-                    <label for="cc-number" class="form-label">Credit card number</label>
-                    <input type="text" class="form-control" id="cc-number" placeholder="" required />
-                    <div class="invalid-feedback">
-                      Credit card number is required
-                    </div>
-                  </div>
-      
-                  <div class="col-md-3">
-                    <label for="cc-expiration" class="form-label">Expiration</label>
-                    <input type="text" class="form-control" id="cc-expiration" placeholder="" required />
-                    <div class="invalid-feedback">
-                      Expiration date required
-                    </div>
-                  </div>
-      
-                  <div class="col-md-3">
-                    <label for="cc-cvv" class="form-label">CVV</label>
-                    <input type="text" class="form-control" id="cc-cvv" placeholder="" required />
-                    <div class="invalid-feedback">
-                      Security code required
-                    </div>
-                  </div>
-                </div>
-      
-                <hr class="my-4"></hr>
-      
-                <button class="w-100 btn btn-primary btn-lg" type="submit">Continue to checkout</button>
-              </form>
-            </div>
-=======
 
                 <div class="col-sm-6">
                   <label for="lastName" class="form-label">
@@ -737,6 +603,7 @@ export function CartView({
                     id="cc-expiration"
                     placeholder=""
                     maxlength="5"
+                    onInput={handleFormatCardExpiration}
                     required
                   ></input>
                   <div class="invalid-feedback">Expiration date required</div>
@@ -752,6 +619,7 @@ export function CartView({
                     id="cc-cvv"
                     placeholder=""
                     maxlength="3"
+                    onInput={handleFormatCVV}
                     required
                   ></input>
                   <div class="invalid-feedback">Security code required</div>
@@ -764,11 +632,10 @@ export function CartView({
                 Order
               </button>
             </form>
->>>>>>> Stashed changes
           </div>
         </div>
         <footer class="my-5 pt-5 text-body-secondary text-center text-small">
-          <p class="mb-1">&copy; 2017–2024 Company Name</p>
+          <p class="mb-1">&copy; 2024–2025 N&N</p>
           <ul class="list-inline">
             <li class="list-inline-item">
               <a href="#">Privacy</a>
@@ -782,11 +649,6 @@ export function CartView({
           </ul>
         </footer>
       </div>
-<<<<<<< Updated upstream
-    );
-};
-=======
     </div>
   );
 }
->>>>>>> Stashed changes
